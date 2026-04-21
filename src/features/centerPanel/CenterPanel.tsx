c8a0162a352342361f7f6ae79db76753562268a9
RedCap-42 <simon.farina45@gmail.com>
2026-04-21 18:38:11 +0200

    amélioration 3

--- src/features/centerPanel/CenterPanel.tsx
+++ src/features/centerPanel/CenterPanel.tsx
@@ -1,18 +1,13 @@
-import { defaultAnalysisHoverCards, type AnalysisHoverCardData } from './components/AnalysisHoverCards';
 import { CenterPanelAnalysis } from './components/CenterPanelAnalysis';
 import { CenterPanelSummary } from './components/CenterPanelSummary';
 import './styles/index.css';
 
-interface CenterPanelProps {
-  analysisHoverCards?: AnalysisHoverCardData[];
-}
-
-export function CenterPanel({ analysisHoverCards = defaultAnalysisHoverCards }: CenterPanelProps) {
+export function CenterPanel() {
   return (
     <aside className="rvc-center-panel" aria-label="Panneau central d'analyse">
       <CenterPanelSummary />
       <div className="rvc-center-panel__divider" />
-      <CenterPanelAnalysis hoverCards={analysisHoverCards} />
+      <CenterPanelAnalysis />
     </aside>
   );
 }
