2ee1be329f81ec3ed3bb726f002cc1567a8f488b
RedCap-42 <simon.farina45@gmail.com>
2026-04-22 08:40:35 +0200

    amélioration

--- src/features/centerPanel/components/CenterPanelToolbar.tsx
+++ src/features/centerPanel/components/CenterPanelToolbar.tsx
@@ -182,15 +182,6 @@ export function CenterPanelToolbar() {
             <IconChevronDown size={16} />
           </button>
 
-          <button
-            className="rvc-center-toolbar__button rvc-center-toolbar__button--compact-accent"
-            type="button"
-            aria-label="Analyser"
-            title="Analyser"
-          >
-            <span className="rvc-center-toolbar__button-text">Analyser</span>
-          </button>
-
           <button className="rvc-center-toolbar__button rvc-center-toolbar__button--label" type="button" aria-label="Tracer" title="Tracer">
             <IconPencilLine />
             <span className="rvc-center-toolbar__button-text">Tracer</span>
